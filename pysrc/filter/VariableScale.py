648d84f6a45e7b73bade1024103e6023e1055daf
LiuShuhao <liushuhao@hust.edu.cn>
2022-11-12 18:07:48 +0800

    first commit

--- pysrc/filter/VariableScale.py
+++ pysrc/filter/VariableScale.py
@@ -16,7 +16,7 @@ def getPsi(sp, lat, lon):
     colat_pie = np.pi / 2 - np.radians(np.arange(-90 + sp / 2, 90 + sp / 2, sp))
     delta_lat = colat_pie - colat
 
-    lon_pie = np.radians(np.arange(-180, 180, sp))
+    lon_pie = np.radians(np.arange(-180 + sp / 2, 180 + sp / 2, sp))
     delta_lon = lon - lon_pie
 
     colat_pie, lon_pie = np.meshgrid(colat_pie, lon_pie)
@@ -29,24 +29,22 @@ def getPsi(sp, lat, lon):
     return Psi
 
 
-class VariableScale():
+class VariableScale:
     """
     This class is to smooth grids by applying a spatial convolution with a variable-scale anisotropy Gaussian kernel.
     """
 
     def __init__(self, r_min, r_max=None, sigma=None, harmonic: Harmonic = None,
                  vary_radius_mode: VaryRadiusWay = VaryRadiusWay.sin2):
-
         if r_max is None:
             r_max = r_min
 
         if sigma is None:
-            sigma = np.mat([[1, 0], [0, 1]])
+            sigma = np.array([[1, 0], [0, 1]])
 
         self.r_min = r_min * 1000
         self.r_max = r_max * 1000
         self.sigma = sigma
-
         self.vary_way = vary_radius_mode
 
         self.harmonic = harmonic
@@ -65,7 +63,7 @@ class VariableScale():
         Psi = getPsi(sp, lat, lon)
 
         if self.vary_way == VaryRadiusWay.sin2:
-            r_lat = (self.r_max - self.r_min) * (np.sin(np.pi / 2 - lat)) ** 2 + self.r_min
+            r_lat = (self.r_max - self.r_min) * np.sin(np.pi / 2 - lat) ** 2 + self.r_min
         else:
             r_lat = (self.r_max - self.r_min) * np.sin(np.pi / 2 - lat) + self.r_min
 
@@ -73,21 +71,23 @@ class VariableScale():
 
         a = np.log(2) / (1 - np.cos(alpha_0))
 
-        PsiT_SigmaI_Psi = np.einsum('ijl,lm,ijm->ij', Psi, np.linalg.inv(self.sigma), Psi)
+        if (self.sigma == np.array([[1, 0], [0, 1]])).all():
+            PsiT_SigmaI_Psi = np.einsum('ijl,lm,ijm->ij', Psi, np.array([[1, 0], [0, 1]]), Psi)
+        else:
+            PsiT_SigmaI_Psi = np.einsum('ijl,lm,ijm->ij', Psi, np.linalg.inv(self.sigma), Psi)
 
         weight = a * np.exp(-a * (2 * PsiT_SigmaI_Psi)) / (
                 (1 - np.exp(-2 * a)) * 2 * np.pi * np.sqrt(np.linalg.det(self.sigma)))
 
         return weight
 
-    def apply_to(self, *params, option=0):
+    def apply_to(self, *params: np.ndarray, option=0):
         """
 
         :param params: SHC Cqlm and Sqlm if option=0, else grids.
         :param option:
         :return:
         """
-        params = np.array(params)
         if option == 0:
             assert len(params) == 2
             assert self.harmonic is not None
@@ -97,6 +97,8 @@ class VariableScale():
         else:
             assert len(params) == 1
             Gqij = params[0]
+
+        input_is_numpy = False
 
         grid_space = 180 / np.shape(Gqij)[1]
 
@@ -119,7 +121,7 @@ class VariableScale():
             Wipq[i] = Wpq
 
         for j in range(length_of_lon):
-            print('\rvs filtering {}/{}'.format(j + 1, length_of_lon), end='\t')
+            print('\rfiltering {}/{}'.format(j + 1, length_of_lon), end='\t')
 
             Gqij_pie = np.zeros_like(Gqij)
             Gqij_pie[:, :, :length_of_lon - j], Gqij_pie[:, :, length_of_lon - j:] = Gqij[:, :, j:], Gqij[:, :, :j]
