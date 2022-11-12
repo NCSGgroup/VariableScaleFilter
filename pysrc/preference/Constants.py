8447591b16815abca83020568bba966313658908
LiuShuhao <liushuhao@hust.edu.cn>
2022-11-12 18:00:51 +0800

    first commit

--- pysrc/preference/Constants.py
+++ pysrc/preference/Constants.py
@@ -2,14 +2,6 @@ class GeoConstants:
     density_earth = 5517  # unit[kg/m3]
     radius_earth = 6378136.3  # unit[m]
 
-    """gas constant for dry air"""
-    Rd = 287.00
-    # Rd = 287.06
-
-    '''gravity constant g defined by WMO'''
-    g_wmo = 9.80665
-    # g_wmo = 9.7
-
     ''' water density'''
     density_water = 1000.0
     # density_water = 1025.0
